f73055ffe6e2f459350610c62d3e0f2a6ed7bc48
Nuwantha <hnpkdias@gmail.com>
2025-03-26 23:09:41 +0530

    feat: data models

---
+++ src/main/java/com/replp/model/File.java
@@ -0,0 +1,86 @@
+package com.replp.model;
+
+import java.time.LocalDateTime;
+
+public class File {
+    private String _id;
+    private String name;
+    private String path;
+    private long size;
+    private String type;
+    private LocalDateTime createdAt;
+
+    public File() {
+    }
+
+    public File(String _id, String name, String path, long size, String type, LocalDateTime createdAt) {
+        this._id = _id;
+        this.name = name;
+        this.path = path;
+        this.size = size;
+        this.type = type;
+        this.createdAt = createdAt;
+    }
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public void setSize(long size) {
+        this.size = size;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "File{" +
+                "_id='" + _id + '\'' +
+                ", name='" + name + '\'' +
+                ", path='" + path + '\'' +
+                ", size=" + size +
+                ", type='" + type + '\'' +
+                ", createdAt=" + createdAt +
+                '}';
+    }
+}
+
+
--- src/main/java/com/replp/model/Property.java
+++ src/main/java/com/replp/model/Property.java
@@ -1,4 +1,95 @@
 package com.replp.model;
 
+import java.util.List;
+
 public class Property {
+    private String _id;
+    private String location;
+    private double price;
+    private double size;
+    private String description;
+    private List<File> images;
+    private String userId;
+
+    public Property() {
+    }
+
+    public Property(String _id, String location, double price, double size, String description, List<File> images, String userId) {
+        this._id = _id;
+        this.location = location;
+        this.price = price;
+        this.size = size;
+        this.description = description;
+        this.images = images;
+        this.userId = userId;
+    }
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getSize() {
+        return size;
+    }
+
+    public void setSize(double size) {
+        this.size = size;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<File> getImages() {
+        return images;
+    }
+
+    public void setImages(List<File> images) {
+        this.images = images;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    @Override
+    public String toString() {
+        return "Property{" +
+                "_id='" + _id + '\'' +
+                ", location='" + location + '\'' +
+                ", price=" + price +
+                ", size=" + size +
+                ", description='" + description + '\'' +
+                ", images=" + images +
+                ", userId='" + userId + '\'' +
+                '}';
+    }
 }
