737812422d75cc28f0a8b064ca54f93a7518e524
Nuwantha <hnpkdias@gmail.com>
2025-03-29 17:52:37 +0530

    feat(UserDAO): implement methods for reading and writing users

--- src/main/java/com/replp/dao/UserDAO.java
+++ src/main/java/com/replp/dao/UserDAO.java
@@ -1,4 +1,19 @@
 package com.replp.dao;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.replp.model.User;
+import com.replp.util.FileNames;
+import com.replp.util.JsonFileActions;
+
+import java.util.List;
+
 public class UserDAO {
+   final JsonFileActions  jsonFileActions = new JsonFileActions(System.getProperty("user.home") + "/.replp/"+ FileNames.USERS);
+
+    public List<User> readUsers() {
+        return jsonFileActions.readJsonFile(new TypeReference<List<User>>() {});
+    }
+    public boolean writeUsers(User users) {
+        return jsonFileActions.writeJsonFile(users, User.class);
+    }
 }
