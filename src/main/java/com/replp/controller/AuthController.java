3355f1bc690c47b29b49b12deada511ee5477bc2
Nuwantha <hnpkdias@gmail.com>
2025-03-29 09:24:01 +0530

    feat: initializing AuthController.java and UserServices.java

---
+++ src/main/java/com/replp/controller/AuthController.java
@@ -0,0 +1,18 @@
+package com.replp.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/auth/login"}, name = "authController", description = "Authentication Controller")
+public class AuthController extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
