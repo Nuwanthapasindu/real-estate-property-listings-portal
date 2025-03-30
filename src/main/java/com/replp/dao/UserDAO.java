1e5c875cf3ca45b3c30abbd4aac9e679a299be96
Nuwantha <hnpkdias@gmail.com>
2025-03-30 16:57:01 +0530

    fix(UserDAO): authentication DAO doc strings and return type

--- src/main/java/com/replp/dao/UserDAO.java
+++ src/main/java/com/replp/dao/UserDAO.java
@@ -11,15 +11,33 @@ import java.util.Optional;
 public class UserDAO {
    final JsonFileActions  jsonFileActions = new JsonFileActions(System.getProperty("user.home") + "/.replp/"+ FileNames.USERS);
 
+    /**
+     * Reads the list of users from the file containing the list of users.
+     *
+     * @return the list of users
+     */
     public List<User> readUsers() {
         return jsonFileActions.readJsonFile(new TypeReference<List<User>>() {});
     }
 
+    /**
+     * Finds a user by the given id.
+     *
+     * @param id the id of the user to find
+     * @return an Optional containing the user if found, empty otherwise
+     */
     public Optional<User> findUserById(String id) {
         List<User> users = readUsers();
-        return users.stream().filter(user -> user.get_id().equals(id)).findFirst();
+        User findUuser = users.stream().filter(user -> user.get_id().equals(id)).findFirst().orElse(null);
+        return Optional.ofNullable(findUuser);
     }
 
+    /**
+     * Writes the given user to the file containing the list of users.
+     *
+     * @param users the user to write
+     * @return true if the write is successful, false otherwise
+     */
     public boolean writeUser(User users) {
         return jsonFileActions.writeJsonFile(users, User.class);
     }
