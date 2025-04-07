18a84d329ebfac303e335bbbae872b9857eea50d
Nuwantha <hnpkdias@gmail.com>
2025-04-07 22:59:19 +0530

    feat: sample User class

--- src/main/java/com/replp/model/User.java
+++ src/main/java/com/replp/model/User.java
@@ -1,5 +1,97 @@
 package com.replp.model;
 
-public class User {
+import java.util.Objects;
 
+public class User {
+    private String _id;
+    private String firstName;
+    private  String lastName;
+    private String contactNumber;
+    private String email;
+    private String password;
+
+    public User() {
+    }
+
+    public User(String _id, String firstName, String lastName, String contactNumber, String email, String password) {
+        this._id = _id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.contactNumber = contactNumber;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true; // Same object reference
+        if (o == null || getClass() != o.getClass()) return false; // Null or different class
+        User user = (User) o;
+        return Objects.equals(_id, user._id); // Compare based on _id
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(_id); // Hash code based on _id
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "_id='" + _id + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", contactNumber='" + contactNumber + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
