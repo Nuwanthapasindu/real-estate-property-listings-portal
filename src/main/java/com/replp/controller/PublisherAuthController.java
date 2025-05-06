690f2c69790c8d387ca124d2694918f366721de4
Janiduloneth <demelj90@gmail.com>
2025-05-06 11:33:19 +0530

    implement controller method for publisher signup

--- src/main/java/com/replp/controller/PublisherAuthController.java
+++ src/main/java/com/replp/controller/PublisherAuthController.java
@@ -8,8 +8,11 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet(urlPatterns = {"/auth/login"}, name = "authController", description = "Authentication Controller")
-public class AuthController extends HttpServlet {
+@WebServlet(urlPatterns = {"/auth/publisher/register"}, name = "PublisherAuthController", description = "Publisher Authentication Controller")
+public class PublisherAuthController extends HttpServlet {
+
+    final PublisherAuthService publisherAuthService = new PublisherAuthService();
+
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
