6b4ceaeb146a04def42ce4783c2a3396b2c7b655
Janiduloneth <demelj90@gmail.com>
2025-05-06 14:41:19 +0530

    sign up controler method

--- src/main/java/com/replp/controller/PublisherAuthController.java
+++ src/main/java/com/replp/controller/PublisherAuthController.java
@@ -1,5 +1,9 @@
 package com.replp.controller;
 
+import com.replp.model.User;
+import com.replp.services.PublisherAuthService;
+import com.replp.util.PasswordHash;
+import com.replp.util.UUIDGenerator;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -16,6 +20,51 @@ public class PublisherAuthController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String firstName = req.getParameter("firstName");
+        String lastName = req.getParameter("lastName");
+        String email = req.getParameter("email");
+        String phone = req.getParameter("phone");
+        String password = req.getParameter("password");
+        String confirmPassword = req.getParameter("confirmPassword");
+
+
+        if (firstName == null || firstName.isEmpty()) {
+            resp.sendRedirect(req.getContextPath() +"/auth/publisher/signup.jsp?error=First Name is required");
+            return;
+        }
+
+        if (lastName == null || lastName.isEmpty()) {
+            resp.sendRedirect(req.getContextPath() +"/auth/publisher/signup.jsp?error=Last Name is required");
+            return;
+        }
+
+        if (!email.matches("^[\\w.-]+@[\\w.-]+\\.\\w{2,}$")) {
+            resp.sendRedirect(req.getContextPath() +"/auth/publisher/signup.jsp?error=Invalid Email");
+            return;
+        }
+
+        if (!phone.matches("\\d{10}")) {
+            resp.getWriter().write("Contact number must be 10 digits");
+            resp.sendRedirect(req.getContextPath() +"/auth/publisher/signup.jsp?error=Contact number must be 10 digits");
+            return;
+        }
+
+        if (!password.equals(confirmPassword)){
+            resp.sendRedirect(req.getContextPath() +"/auth/publisher/signup.jsp?error=Password mis match");
+            return;
+        }
+
+        User user = new User(UUIDGenerator.generate(),firstName,lastName,phone,email, PasswordHash.hashPassword(password));
+        try {
+            boolean isRegisterSuccess = publisherAuthService.registerUser(user);
+            if (isRegisterSuccess){
+                resp.sendRedirect(req.getContextPath()+"/auth/publisher/login.jsp");
+            }
+        } catch (Exception e) {
+            resp.sendRedirect("auth/signup.jsp?error = Something Went wrong" );
+        }
+
 
     }
 }
