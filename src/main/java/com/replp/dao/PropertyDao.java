ad9c566f5e7141d51813bdc3b0cf80dfd82ba72b
chamod778 <it24101098@my.sliit.lk>
2025-05-11 11:26:49 -0700

    Update Dao according to BST

--- src/main/java/com/replp/dao/PropertyDao.java
+++ src/main/java/com/replp/dao/PropertyDao.java
@@ -1,6 +1,7 @@
 package com.replp.dao;
 
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.replp.DSA.PropertyBST;
 import com.replp.model.Property;
 import com.replp.util.FileNames;
 import com.replp.util.JsonFileActions;
@@ -10,23 +11,34 @@ import java.util.List;
 
 public class PropertyDao {
     private final String FILE_PATH;
-    private JsonFileActions jsonFileActions;
+    private final JsonFileActions jsonFileActions;
+    private final PropertyBST propertyBST;
 
     public PropertyDao() {
         FILE_PATH = System.getProperty("user.home") + "/.replp/"+ FileNames.PROPERTIES;
         jsonFileActions = new JsonFileActions(FILE_PATH);
-        System.out.println(FILE_PATH);
+        propertyBST=new PropertyBST();
+        List<Property> properties= jsonFileActions.readJsonFile(new TypeReference<List<Property>>() {});
+        for (Property property:properties){
+            propertyBST.insert(property);
+        }
     }
 
     public List<Property> getAllProperties() {
-        return jsonFileActions.readJsonFile(new TypeReference<List<Property>>() {});
+       return propertyBST.inOrderTraversal();
     }
 
 
 
 
     public boolean writeProperty(Property property){
+        propertyBST.insert(property);
         return  jsonFileActions.writeJsonFile(property);
+    }
+    public boolean deleteProperty( int index,String ID){
+        propertyBST.deleteById(ID);
+
+        return  jsonFileActions.deleteJsonFile(index);
     }
 
 
