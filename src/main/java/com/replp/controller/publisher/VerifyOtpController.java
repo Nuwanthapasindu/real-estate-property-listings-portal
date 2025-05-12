fab700de4bae851c1cc806e54a333007375847a4
PamudiManjarie <pamudimanjarie@gmail.com>
2025-05-12 21:49:22 +0530

    fix: remove unnecessary Stringbuilder lines

--- src/main/java/com/replp/controller/publisher/VerifyOtpController.java
+++ src/main/java/com/replp/controller/publisher/VerifyOtpController.java
@@ -17,24 +17,12 @@ import java.io.IOException;
         @Override
         protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
             String inputOtp = req.getParameter("otp");
-            String otp1 = req.getParameter("otp1");
-            String otp2 = req.getParameter("otp2");
-            String otp3 = req.getParameter("otp3");
-            String otp4 = req.getParameter("otp4");
-            String otp5 = req.getParameter("otp5");
-            String otp6 = req.getParameter("otp6");
 
             HttpSession session = req.getSession();
             String email = (String) session.getAttribute("email");
             String sessionOtp = (String) session.getAttribute("otp");
-            StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.append(otp1);
-            stringBuilder.append(otp2);
-            stringBuilder.append(otp3);
-            stringBuilder.append(otp4);
-            stringBuilder.append(otp5);
-            stringBuilder.append(otp6);
-            if (stringBuilder.toString().equals(sessionOtp)) {
+
+            if (inputOtp.equals(sessionOtp)) {
                 resp.sendRedirect(req.getContextPath()+"/auth/publisher/resetPassword.jsp");
             } else {
                 resp.sendRedirect(req.getContextPath()+"/auth/publisher/verifyOTP.jsp?error=Invalid OTP. Try again.");
