5ffc366488e4044d8dba2f35c667e2ac2c0e0946
Nuwantha <hnpkdias@gmail.com>
2025-05-12 19:17:18 +0530

    Add LogoutController to handle user logout functionality
    Introduced a new servlet `LogoutController` to manage user logout processes. The controller invalidates the current session and redirects users to the login page.

---
+++ src/main/java/com/replp/controller/publicUser/LogoutController.java
@@ -0,0 +1,17 @@
+package com.replp.controller.publicUser;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class LogoutController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getSession().invalidate();
+        resp.sendRedirect(req.getContextPath() + "/auth/public/login.jsp");
+    }
+}
