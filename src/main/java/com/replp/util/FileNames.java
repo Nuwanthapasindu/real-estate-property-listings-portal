68fbb22edf95de751ad6c066137f403292dc85df
Nuwantha <hnpkdias@gmail.com>
2025-05-12 17:13:43 +0530

    Add method to generate file paths in FileNames utility
    Introduced `generatePath` to simplify constructing file paths by appending filenames to the user's home directory and the `.replp` folder. This enhances flexibility and reduces redundancy in path creation across the application.

--- src/main/java/com/replp/util/FileNames.java
+++ src/main/java/com/replp/util/FileNames.java
@@ -4,4 +4,8 @@ public class FileNames {
     public static final String USERS = "PublicUsers.json";
     public static final String PUBLISHERS = "Publishers.json";
     public static final String PROPERTIES = "properties.json";
+
+    public static String generatePath(String fileName) {
+        return System.getProperty("user.home") + "/.replp/"+ fileName;
+    }
 }
