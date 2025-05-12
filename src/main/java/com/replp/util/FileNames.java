d3192f58d3739044d2f72bcffdc98244eb78d83f
Nuwantha <hnpkdias@gmail.com>
2025-05-12 16:49:30 +0530

    Update file constants in FileNames.java
    Renamed the "USERS" file to "PublicUsers.json" for clarity and added a new constant "PUBLISHERS" for "Publishers.json". This improves organization and supports new functionality related to publishers.

--- src/main/java/com/replp/util/FileNames.java
+++ src/main/java/com/replp/util/FileNames.java
@@ -1,6 +1,7 @@
 package com.replp.util;
 
 public class FileNames {
-    public static final String USERS = "users.json";
+    public static final String USERS = "PublicUsers.json";
+    public static final String PUBLISHERS = "Publishers.json";
     public static final String PROPERTIES = "properties.json";
 }
